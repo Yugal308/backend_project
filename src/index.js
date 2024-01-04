aabf0fed33caa687949e1ec91aa6aec035306f09
Yugal308 <yverma_bemba19@thapar.edu>
2024-01-05 00:12:13 +0530

    added user and video model

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 // require('dotenv').config()
 import dotenv from "dotenv"
 import connectDB from './db/index.js'
+import { app } from "./app.js"
 
 dotenv.config()
 
 connectDB()
+.then(()=>{
+    app.on("error",(err)=>{
+        console.log("ERRR: ",err);
+        throw err;
+    })
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("MongoDB Connection failed",error);
+})
